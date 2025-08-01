f32068cdfa7b9fa9791423f8077a6d34412544aa
Tanggogrich <vakhtanggogrich@gmail.com>
2025-08-01 14:25:29 +0200

    resolver: - added TODO for further code improvement and optimization

--- lox/Resolver.java
+++ lox/Resolver.java
@@ -5,6 +5,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
+// TODO: Our resolver calculates which environment the variable is found in, but it’s still looked up by name in that map.
+//  A more efficient environment representation would store local variables in an array and look them up by index.
+//  Extend the resolver to associate a unique index for each local variable declared in a scope.
+//  When resolving a variable access, look up both the scope the variable is in and its index and store that.
+//  In the interpreter, use that to quickly access a variable by its index instead of using a map.
 public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private final Interpreter interpreter;
     private final Stack<Map<String, VariableStatus>> scopes = new Stack<>();
